90c8adc1598ebf8362dd5a44d2acef9f077e7bf2
mithelan <mithelandev@gmail.com>
2020-06-29 19:58:10 +0530

    Final Navigation

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -8,40 +8,17 @@ class Navbar extends Component {
         return (
             <div className='container'>
 
-                <div className='arrange'>
 
-                <nav className="navbar navbar-expand-lg navbar-light bg-danger">
-                    <a className="navbar-brand" href="#">BEAT THE EATERS |</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul className="navbar-nav">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item ">
-                                <a className="nav-link" href="/lk">Sri Lanka <span className="sr-only"></span></a>
-                            </li>
+                    <ul>
+                        <li><a className="active" href='/'>Home</a></li>
+                        <li><a href="/">Sri Lanka</a></li>
+                        <li><a href="/pay">Donation</a></li>
+                        <li><a href="/login">Sign In</a></li>
+                        <li><a href="/contact">Contact us</a></li>
+                    </ul>
 
-                            <li className="nav-item">
-                                <a className="nav-link" href='/pay'>Help the needy</a>
-                            </li>
-                            <li className="nav-item ">
-                                <a className="nav-link" href="/contact">Contact us<span className="sr-only"></span></a>
-                            </li>
-                            
-                            <li className="nav-item ">
-                                <a className="nav-link" href="/login">Sign In<span className="sr-only"></span></a>
-                            </li>
-
-                        </ul>
-                    </div>
-                </nav>
                 </div>
-            </div>
+
         );
     }
 }
